4739f1f6a9cb587c017dcfe75913d07095521f71
esnilara <esteban@esnilara.io>
2018-03-01 20:38:35 -0600

    Feature[conekta-service]: Create conekta service based on conekta.js basic methods

---
+++ addon/services/conekta.js
@@ -0,0 +1,121 @@
+/* global Conekta */
+import loadScript from 'ember-conekta-service/utils/load-script';
+import RSVP from 'rsvp';
+import Ember from 'ember';
+import Service from '@ember/service';
+import { get, set } from '@ember/object';
+import { readOnly } from '@ember/object/computed';
+import { registerWaiter } from '@ember/test';
+import { debug } from '@ember/debug';
+import { isEqual, typeOf } from '@ember/utils';
+
+const { Logger } = Ember;
+
+export default Service.extend({
+  didConfigure: false,
+  config: null,
+  runCount: 0,
+  lazyLoad: readOnly('config.lazyLoad'),
+  mock: readOnly('config.mock'),
+  publicKey: readOnly('config.publicKey'),
+  debuggingEnabled: readOnly('config.debug'),
+
+  init() {
+    this._super(...arguments);
+
+    let lazyLoad = get(this, 'lazyLoad');
+    let mock = get(this, 'mock');
+
+    if (Ember.testing)  {
+      this._waiter = () => {
+        return get(this, 'runCount') === 0;
+      };
+
+      registerWaiter(this._waiter);
+    }
+
+    if (!lazyLoad || mock) {
+      this.configure();
+    }
+  },
+
+  load() {
+    let lazyLoad = get(this, 'lazyLoad');
+    let mock = get(this, 'mock');
+
+    let loadJs = lazyLoad && !mock ?
+      loadScript('https://cdn.conekta.io/js/latest/conekta.js') :
+      RSVP.resolve();
+
+    return loadJs.then(() => {
+      this.configure();
+    });
+  },
+
+  configure() {
+    let didConfigure = get(this, 'didConfigure');
+
+    if (!didConfigure) {
+      let publicKey = get(this, 'publicKey');
+
+      Conekta.setPublicKey(publicKey);
+
+      this.card = {
+        createToken: this._createCardToken.bind(this)
+      };
+
+      this._checkForAndAddCardFn('getBrand', Conekta.card.getBrand);
+      this._checkForAndAddCardFn('validateNumber', Conekta.card.validateNumber);
+      this._checkForAndAddCardFn('validateExpirationDate', Conekta.card.validateExpirationDate);
+      this._checkForAndAddCardFn('validateCVC', Conekta.card.validateCVC);
+
+      set(this, 'didConfigure', true);
+    }
+  },
+
+  debug(...args) {
+    let debuggingEnabled = get(this, 'debuggingEnabled');
+
+    if (debuggingEnabled) {
+      args[0] = `ConektaService: ${args[0]}`;
+      Logger.info.apply(null, args);
+    }
+  },
+
+  conektaPromise(callback) {
+    return this.load().then(() => {
+      return new RSVP.Promise((resolve, reject) => {
+        callback(resolve, reject);
+      });
+    });
+  },
+
+  _createCardToken(card) {
+    debug('card.createToken:', card);
+
+    this.incrementProperty('runCount');
+
+    return this.conektaPromise((resolve, reject) => {
+      Conekta.Token.create(card, (status, response) => {
+        debug('Conekta.Token.create handler - status %s, response:', status, response);
+
+        if (response.error) {
+          reject(response);
+        } else {
+          resolve(response);
+        }
+
+        this.decrementProperty('runCount');
+      });
+    });
+  },
+
+  _checkForAndAddCardFn(name, fn) {
+    if (isEqual(typeOf(Conekta.card[name]), 'function')) {
+      this.card[name] = fn;
+    } else {
+      this.card[name] = () => {};
+      Logger.error(`ember-conekta-service: ${name} on Conekta.card is no longer available`);
+    }
+  }
+});
