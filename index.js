5ccc3c918eb342df17341c63647547b7d24ae962
esnilara <esteban@esnilara.io>
2018-03-01 20:37:48 -0600

    Feature[conekta-service]: Add conekta library to index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-conekta-service'
+  name: 'ember-conekta-service',
+
+  contentFor: function(type, config) {
+    let conektaConfig = config.conekta || {};
+
+    let lazyLoad = conektaConfig.lazyLoad;
+    let mock = conektaConfig.mock;
+
+    if (type === 'body' && !lazyLoad && !mock) {
+      return '<script type="text/javascript" src="https://cdn.conekta.io/js/latest/conekta.js"></script>';
+    }
+  },
+
+  isDevelopingAddon() {
+    return false;
+  }
 };
